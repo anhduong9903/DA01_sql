819bef33c4457fac5caf01b8e45963e3abaa2239
anhduong9903 <156098210+anhduong9903@users.noreply.github.com>
2024-01-09 23:58:25 +0700

    practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -1,1 +1,38 @@
-
+-- ex1
+SELECT NAME FROM CITY
+WHERE POPULATION > 120000 AND COUNTRYCODE ='USA'; 
+-- ex2
+SELECT * FROM CITY 
+WHERE COUNTRYCODE='JPN';
+-- ex3
+SELECT CITY, STATE FROM STATION; 
+-- ex4
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%';
+-- ex5
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u';
+-- ex6
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY NOT LIKE 'a%' AND CITY NOT LIKE 'e%' AND CITY NOT LIKE 'i%' AND CITY NOT LIKE 'o%' AND CITY NOT LIKE 'u%';
+-- ex7
+SELECT name FROM Employee
+ORDER BY name ASC; 
+-- ex8 
+SELECT name FROM Employee 
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id ASC;
+-- ex9
+SELECT product_id FROM Products
+WHERE low_fats='Y' AND recyclable='Y'; 
+-- ex10
+SELECT name FROM Customer
+WHERE referee_id <> 2 OR referee_id IS NULL; 
+-- ex11
+SELECT name, population, area FROM WORLD 
+WHERE area >= 3000000 OR population >= 25000000; 
+-- ex12
+SELECT DISTINCT author_id AS id FROM Views 
+WHERE viewer_id = author_id AND viewer_id >= 1
+ORDER BY id ASC;
+-- ex13
